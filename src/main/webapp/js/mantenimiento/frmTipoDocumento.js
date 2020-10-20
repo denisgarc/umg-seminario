45f6e30544b398bafced542ba1ff1dffa2b14f3c
Dennis Florencio Oxlaj <denisgarc@hotmail.com>
2020-10-20 06:03:48 -0600

    chore: Se cambian ids de los objetos comunes, para que tomen valor de variables globales.

--- src/main/webapp/js/mantenimiento/frmTipoDocumento.js
+++ src/main/webapp/js/mantenimiento/frmTipoDocumento.js
@@ -5,16 +5,21 @@ var newRecord = {
     activo: 'S'
 };
 
+var $modalId = 'mainTipoDocModal';
+var $formId = 'frmTipoDocumentoMant';
+var $tableId = 'tblTipoDocumento';
+var $controller = 'TipoDocumentoController';
+
 $(document).ready(function (e) {
 
-    $('#tblTipoDocumento tbody').on('click', 'button', function () {
+    $(`#${$tableId} tbody`).on('click', 'button', function () {
         var action = this.attributes.getNamedItem("data-action").value;
-        var data = $('#tblTipoDocumento').DataTable().row($(this).parents('tr')).data();
+        var data = $(`#${$tableId}`).DataTable().row($(this).parents('tr')).data();
 
         switch (action) {
             case "edit":
                 loadData(data);
-                $('#mainTipoDocModal').modal({
+                $(`#${$modalId}`).modal({
                     keyboard: false,
                     focus: true
                 });
@@ -36,15 +41,19 @@ $(document).ready(function (e) {
 
 function loadData(selectedItem) {
     Object.keys(selectedItem).forEach((key) => {
-        $(`#` + key).val(selectedItem[key]);
+        if(key == 'activo'){
+            $('input[name="' + key + '"][value="' + selectedItem[key] + '"]').attr('checked',true);
+        } else {
+            $(`#` + key).val(selectedItem[key]);
+        }
     });
 }
 
 function save(selectedItem, action = 'N') {
-    var payload = selectedItem == undefined ? $('#frmTipoDocumentoMant').serialize() : selectedItem;
-    if (ValidarFormulario('frmTipoDocumentoMant') || action == 'E') {
+    var payload = selectedItem == undefined ? $(`#${$formId}`).serialize() : selectedItem;
+    if (ValidarFormulario(`${$formId}`) || action == 'E') {
         Request(payload).then((result) => {
-            CloseModal('mainTipoDocModal');
+            CloseModal(`${$modalId}`);
             ShowSuccessDialog("Proceso finalizado correctamente", () => {
                 location.reload();
             });
@@ -64,7 +73,7 @@ function Request(payload) {
 
             $.ajax({
                 method: 'POST',
-                url: $BaseUrl + 'TipoDocumentoController',
+                url: $BaseUrl + $controller,
                 dataType: 'json',
                 data: payload,
             }).done((result) => {
@@ -83,7 +92,7 @@ function Request(payload) {
 
 function loadNew() {
     loadData(newRecord);
-    $('#mainTipoDocModal').modal({
+    $(`#${$modalId}`).modal({
         keyboard: false,
         focus: true
     });
